20d0a4d62416a347d3c907326bf8ea198a494df5
Kauan Carvalho <kauan3210@gmail.com>
2023-05-30 08:38:46 -0300

    Adição da função que guarda as informações do form

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById("modal")
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const modalForm = document.getElementById("costumer-form")
+const modalText= document.getElementById("modal-text")
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -18,5 +19,32 @@ modalCloseBtn.addEventListener('click',function(){
 modalForm.addEventListener('submit',function(e){
     e.preventDefault()
     console.log("submited")
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>
+    `
+
+    setTimeout(function(){
+        document.getElementById("uploadText").innerText = `Making the sale...`
+    },1500)
+
+    
+    const FormDataCostumer = new FormData(modalForm)
+    const name = FormDataCostumer.get('costumerName')
+
+    setTimeout(function(){
+        document.getElementById("modal-inner").innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${name}</span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate-laughing.gif">
+        </div>
+    ` 
+    },3000)
+
 })
 
