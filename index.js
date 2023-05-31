5233ecc4b90f9037f90a267f67696f6b1ddfbadb
Kauan Carvalho <kauan3210@gmail.com>
2023-05-31 07:13:33 -0300

    Project Finished

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
-const modal = document.getElementById("modal")
+const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
-const modalForm = document.getElementById("costumer-form")
-const modalText= document.getElementById("modal-text")
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
 setTimeout(function(){
     modal.style.display = 'inline'
-    },1500)
+}, 1500)
 
-setTimeout(function(){
-    modalCloseBtn.removeAttribute('disabled')
-},5000)
-    
-
-modalCloseBtn.addEventListener('click',function(){
+modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
-}) 
+})
 
-modalForm.addEventListener('submit',function(e){
+declineBtn.addEventListener('mouseenter', function(){
+    modalChoiceBtns.classList.toggle('modal-choice-btns-reverse')
+})
+
+
+
+consentForm.addEventListener('submit', function(e){
     e.preventDefault()
-    console.log("submited")
+    
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
+    
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
-        <p id="uploadText">
-            Uploading your data to the dark web...
-        </p>
-    </div>
-    `
-
-    setTimeout(function(){
-        document.getElementById("uploadText").innerText = `Making the sale...`
-    },1500)
-
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>` 
     
-    const FormDataCostumer = new FormData(modalForm)
-    const name = FormDataCostumer.get('costumerName')
-
     setTimeout(function(){
-        document.getElementById("modal-inner").innerHTML = `
-        <h2>Thanks <span class="modal-display-name">${name}</span>, you sucker! </h2>
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
+    }, 1500)
+    
+    
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
         <p>We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/pirate-laughing.gif">
         </div>
-    ` 
-    },3000)
-
-})
-
+    `
+    modalCloseBtn.disabled = false
+    }, 3000)
+  
+}) 
